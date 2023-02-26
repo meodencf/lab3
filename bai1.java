75a53d96612049a299d0a6baf6217fdbb33a29a5
meodencf <125757693+meodencf@users.noreply.github.com>
2023-02-26 22:41:01 +0700

    add

---
+++ bai1.java
@@ -0,0 +1,51 @@
+package bai1;
+
+public class Circle {
+
+  private double bankinh;
+
+  public Circle(double bankinh) {
+    this.bankinh = bankinh;
+  }
+
+  public double getBankinh() {
+    return bankinh;
+  }
+
+  public void setRadius(double bankinh) {
+    this.bankinh = bankinh;
+  }
+
+  public double getDientich() {
+    return Math.PI * banhkinh * bankinh;
+  }
+
+  public double getDuongtron() {
+    return 2 * Math.PI * bankinh;
+  }
+}
+
+public class hinhvuong {
+
+  private double canh;
+
+  public Square(double canh) {
+    this.canh = canh;
+  }
+
+  public double getCanh() {
+    return canh;
+  }
+
+  public void setSide(double canh) {
+    this.canh = canh;
+  }
+
+  public double getDientich() {
+    return canh * canh;
+  }
+
+  public double getChuvi() {
+    return 4 * Canh;
+  }
+}
